efbf34664636113fa765f27da7dc693c759d8ed5
RafaGorski <rbritto@digitalhub.com.br>
2024-07-17 22:32:14 -0300

    New "edit" button that changes the note

--- controllers/notes/update.php
+++ controllers/notes/update.php
@@ -1,3 +1,43 @@
 <?php
 
-dd('update');
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$db = App::resolve(Database::class);
+
+$currentUserId = 2222;
+
+// find the corresponding note
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [
+    'id' => $_POST['id']
+])->findOrFail();
+
+// authorize that the current user can edit
+authorize($note['user_id'] === $currentUserId);
+
+// validate the form
+$errors = [];
+
+if (! Validator::string($_POST['body'], 1, 1000)) {
+    $errors['body'] = 'Body of no more than 1,000 characters is required.';
+}
+
+// if no validation errors, update the note
+if (count($errors)) {
+    return view('notes/edit.view.php', [
+        'heading' => 'Edit Note',
+        'errors' => $errors,
+        'note' => $note
+    ]);
+}
+
+$db->query('UPDATE notes SET body = :body, title = :title WHERE id = :id', [
+    'id' => $_POST['id'],
+    'body' => $_POST['body'],
+    'title' => $_POST['title']
+]);
+
+// redirerct the user
+header('location: /notes');
+die();
--- views/notes/edit.view.php
+++ views/notes/edit.view.php
@@ -6,6 +6,7 @@
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <form method="POST" action="/note">
             <input type="hidden" name="_method" value="PATCH">
+            <input type="hidden" name="id" value="<?= $note['id'] ?>">
 
             <div class="space-y-12">
                 <div class="border-b border-gray-900/10 pb-12">
@@ -32,7 +33,7 @@
                             <div class="mt-2">
                                 <textarea id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5
                                  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
-                                 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                 focus:ring-2 focus:ring-inset fsocus:ring-indigo-600 sm:text-sm sm:leading-6"
                                           placeholder="Write here you note" required><?= $note['body'] ?></textarea>
                                 <?php if(isset($errors['body'])) : ?>
                                     <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
@@ -52,6 +53,7 @@
 <!--                    <input type="hidden" value="--><?php //= $note['id']; ?><!--" name="id" class="hidden">-->
 <!--                    <button class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Delete</button>-->
 <!--                </form>-->
+
                 <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
             </div>
         </form>
